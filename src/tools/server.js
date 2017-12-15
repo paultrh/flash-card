c2f4a0b905218709541e4945e1980cdcaff6e4d1
paultrh <paultr_h@epita.fr>
2017-12-15 16:25:27 +0100

    Update server.js

--- src/tools/server.js
+++ src/tools/server.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import env from './../../env';
 
 export default {
   makeRequest(method, urlPath, params) {
     const config = {
       method,
-      url: `http://localhost:5000/${urlPath}`,
+      url: `http://${env.IPV4}:5000/${urlPath}`,
       headers: { 'Content-Type': 'application/json' },
       data: params,
     };
